3eb97799595ffd9f3edf3acbf1cb1c03cdc705d7
sherzodboy <kholmamatovsh09@gmail.com>
2024-02-24 10:11:12 +0500

    Intial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// 1ta funksiya yarating u doim doim teskari qiymat qaytaradigan
+
+// 1ta funksiya yarating
+// !
+
+// const reverseFn = function (text) {
+//   return !text;
+// };
+
+// console.log(reverseFn(""));
+// console.log(reverseFn([]));
+// console.log(reverseFn(undefined));
+// console.log(reverseFn(1));
+// console.log(reverseFn("1"));
+
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(Math.max(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+// console.log(Math.min(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// const array = [3, 6, 2, 56, 32, 5, 89, 32];
+// let largest = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > largest) {
+//     largest = array[i];
+//   }
+// }
+
+// // console.log(largest);
+
+// const array1 = ["a", "b", "c"];
+// const array2 = ["d", "e", "f"];
+
+// console.log(array1.concat(array2));
+
+// console.log(num);
+// let num = 10;
+
+// const getTemprature = () => {
+//   const tempAlibek = {
+//     unit: "Celcies",
+//     temp: "Tempratura",
+//     // fix
+//     value: Number(prompt("What is yor tem?")),
+//   };
+
+// find
+//   console.table(tempAlibek);
+
+//   console.log(tempAlibek.value);
+//   console.warn(tempAlibek.value);
+//   console.error(tempAlibek.value);
+
+// error
+//   const alibek = tempAlibek.value + 180;
+//   return alibek;
+// };
+
+// console.log(getTemprature());
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // 45
